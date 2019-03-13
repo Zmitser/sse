c1e9f7a7991a5eba83dd0a309fa89158bd4929a3
Dmitriy Borisovets <gagat@tut.by>
2019-03-13 21:30:17 +0300

    get weather

--- src/main/kotlin/by/zmitser/webapp/serversideevent/ServerSideEventApplication.kt
+++ src/main/kotlin/by/zmitser/webapp/serversideevent/ServerSideEventApplication.kt
@@ -1,10 +1,19 @@
 package by.zmitser.webapp.serversideevent
 
+import org.openweathermap.api.UrlConnectionDataWeatherClient
+import org.openweathermap.api.WeatherClient
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class ServerSideEventApplication
+class ServerSideEventApplication{
+    @Bean
+    fun openWeatherMapService(): WeatherClient = UrlConnectionDataWeatherClient("1d82742ee19d794f79341fab6dac7b50")
+
+
+}
+
 
 fun main(args: Array<String>) {
     SpringApplication.run(ServerSideEventApplication::class.java, *args)
--- src/main/kotlin/by/zmitser/webapp/serversideevent/controller/WeatherController.kt
+++ src/main/kotlin/by/zmitser/webapp/serversideevent/controller/WeatherController.kt
@@ -1,24 +1,35 @@
 package by.zmitser.webapp.serversideevent.controller
 
+import org.openweathermap.api.WeatherClient
+import org.openweathermap.api.query.QueryBuilderPicker
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
-import java.util.*
+import org.openweathermap.api.query.forecast.daily.ByCityName
+import org.openweathermap.api.query.UnitFormat.METRIC
 import java.util.concurrent.Executors.newCachedThreadPool
+import org.openweathermap.api.query.Language.ENGLISH
 
 
 @RestController
 @RequestMapping("/weather")
-class WeatherController {
+class WeatherController(val weatherClient: WeatherClient) {
 
     private val nonBlockingService = newCachedThreadPool()
 
     @GetMapping
     fun emitWeather(): SseEmitter {
         val emitter =  SseEmitter()
+        val build: ByCityName = QueryBuilderPicker.pick()
+                .forecast()
+                .daily()
+                .byCityName("Minsk")
+                .unitFormat(METRIC)
+                .language(ENGLISH)
+                .build()
         nonBlockingService.execute {
-            emitter.send("/sse" + " @ " + Date())
+            emitter.send(weatherClient.getWeatherData(build))
             emitter.complete()
         }
         return emitter
